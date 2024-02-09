740279a13c479f3982b95869b731d2c9b3f31a08
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2024-02-08 22:08:20 -0500

    Implemented ToMe merging for DinoV2! With one exception: proportional attention is ignored for the xFormers mem_efficient_attention operator. See notes in dinov2.py about this

--- hwtome/patch/dinov2.py
+++ hwtome/patch/dinov2.py
@@ -2,8 +2,17 @@ import torch
 
 
 ### DinoV2
-from dinov2.layers.block import Block, NestedTensorBlock, drop_add_residual_stochastic_depth
-from dinov2.layers.attention import Attention, MemEffAttention, XFORMERS_AVAILABLE, XFORMERS_ENABLED
+from dinov2.layers.block import (
+    Block,
+    NestedTensorBlock,
+    drop_add_residual_stochastic_depth,
+)
+from dinov2.layers.attention import (
+    Attention,
+    MemEffAttention,
+    XFORMERS_AVAILABLE,
+    XFORMERS_ENABLED,
+)
 from dinov2.hub.classifiers import _LinearClassifierWrapper
 
 
@@ -37,17 +46,22 @@ except ImportError:
 ### Custom Attention Mechanisms
 ###
 class ToMeDinoV2Attention(Attention):
-    def forward(self, x : torch.Tensor) -> Tuple:
+    def forward(self, x: torch.Tensor, attn_size=None) -> Tuple:
         B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        qkv = (
+            self.qkv(x)
+            .reshape(B, N, 3, self.num_heads, C // self.num_heads)
+            .permute(2, 0, 3, 1, 4)
+        )
 
+        ### Q, K, V have shapes
+        ### B, # heads, # tokens, # features per head
         q, k, v = qkv[0] * self.scale, qkv[1], qkv[2]
         attn = q @ k.transpose(-2, -1)
 
-        ### Lookup attention size
-        attn_size = self._tome_info["size"] if self._tome_info["prop_attn"] else None
         ### Apply proportional attention
         if attn_size is not None:
+            # print(f"attn shape: {attn.shape} and attn bias shape: {attn_size.log()[:, None, None, :, 0].shape}")
             attn = attn + attn_size.log()[:, None, None, :, 0]
 
         attn = attn.softmax(dim=-1)
@@ -62,7 +76,7 @@ class ToMeDinoV2Attention(Attention):
 
 
 class ToMeDinoV2MemEffAttention(Attention):
-    def forward(self, x: torch.Tensor, attn_bias=None) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, attn_bias=None, attn_size=None) -> torch.Tensor:
         if not XFORMERS_AVAILABLE:
             if attn_bias is not None:
                 raise AssertionError("xFormers is required for using nested tensors")
@@ -71,14 +85,32 @@ class ToMeDinoV2MemEffAttention(Attention):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads)
 
+        ### Each has shape B, #tokens, #heads, # features per head
         q, k, v = unbind(qkv, 2)
+
+        ### See https://github.com/facebookresearch/xformers/blob/main/xformers/ops/fmha/__init__.py
+        ### Let's hijack attention bias here!
+        # if attn_size is not None:
+        #     ### NOTE: Need to call repeat so memory_efficient_attention doesn't yell at us!
+        #     ### PyTorch would normally understand what to do in this case, but they have a custom check it would seem
+        #     ### GEEZE
+        #     ### Need to add an extra virtual "token" to bypas issues with memory alignment from memory_efficient_attention(...)
+        #     prop_attn_bias = torch.zeros(size=(B, self.num_heads, N+1, N+1), device=x.device, dtype=x.dtype)
+        #     prop_attn_bias[:,:,:N,:N] += attn_size.log()[:, None, None, :, 0]
+        #     ### Substitute
+        #     if attn_bias is None:
+        #         attn_bias = prop_attn_bias[:,:,:N,:N]
+        #     ### Add bias
+        #     else:
+        #         attn_bias += prop_attn_bias[:,:,:N,:N]
 
         x = memory_efficient_attention(q, k, v, attn_bias=attn_bias)
         x = x.reshape([B, N, C])
 
         x = self.proj(x)
         x = self.proj_drop(x)
-        return x, k.mean(dim=1)
+        ### NOTE: dim=2 is proper for taking mean along head dimension (not tokens!)
+        return x, k.mean(dim=2)
 
 
 ###
@@ -87,7 +119,10 @@ class ToMeDinoV2MemEffAttention(Attention):
 class ToMeDinoV2Block(Block):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         def attn_residual_func(x: torch.Tensor) -> Tuple:
-            attention, metric = self.attn(self.norm1(x))
+            attn_size = (
+                self._tome_info["size"] if self._tome_info["prop_attn"] else None
+            )
+            attention, metric = self.attn(self.norm1(x), attn_size=attn_size)
             attention_scaled = self.ls1(attention)
             return attention_scaled, metric
 
@@ -95,8 +130,10 @@ class ToMeDinoV2Block(Block):
             return self.ls2(self.mlp(self.norm2(x)))
 
         if self.training and self.sample_drop_ratio > 0.1:
-            # Raise error 
-            raise AssertionError("ToMe wrapping does not support the drop_add_residaul_stochastic_depth flag during training")
+            # Raise error
+            raise AssertionError(
+                "ToMe wrapping does not support the drop_add_residaul_stochastic_depth flag during training"
+            )
 
             # the overhead is compensated only for a drop path rate larger than 0.1
             x = drop_add_residual_stochastic_depth(
@@ -127,10 +164,15 @@ class ToMeDinoV2Block(Block):
                     self._tome_info["source"] = merge_source(
                         merge, x, self._tome_info["source"]
                     )
-                x, self._tome_info["size"] = merge_wavg(merge, x, self._tome_info["size"])
+                    x, self._tome_info["size"] = merge_wavg(
+                        merge, x, self._tome_info["size"]
+                    )
 
             x = x + self.drop_path1(ffn_residual_func(x))  # FIXME: drop_path2
         else:
+            # attn_size = self._tome_info["size"][:, None, None, :, 0].shape if self._tome_info["size"] is not None else "None"
+            # print(f"x shape prior: {x.shape} and attn size: {attn_size}")
+
             residual, metric = attn_residual_func(x)
             x = x + residual
 
@@ -150,7 +192,11 @@ class ToMeDinoV2Block(Block):
                     self._tome_info["source"] = merge_source(
                         merge, x, self._tome_info["source"]
                     )
-                x, self._tome_info["size"] = merge_wavg(merge, x, self._tome_info["size"])
+                x, self._tome_info["size"] = merge_wavg(
+                    merge, x, self._tome_info["size"]
+                )
+
+            # print(f"x shape post: {x.shape}")
 
             x = x + ffn_residual_func(x)
         return x
@@ -162,18 +208,20 @@ class ToMeDinoV2NestedTensorBlock(ToMeDinoV2Block):
             return super().forward(x_or_x_list)
         elif isinstance(x_or_x_list, list):
             ### NOTE: Not supported yet
-            raise AssertionError("ToMe wrapping does not support list of tensors / nested tensors")
+            raise AssertionError(
+                "ToMe wrapping does not support list of tensors / nested tensors"
+            )
             if not XFORMERS_AVAILABLE:
                 raise AssertionError("xFormers is required for using nested tensors")
             return self.forward_nested(x_or_x_list)
         else:
             raise AssertionError
-        
+
 
 ###
 ### Factory ToMe DinoV2 Generator
 ###
-def make_tome_class(transformer_class : _LinearClassifierWrapper):
+def make_tome_class(transformer_class: _LinearClassifierWrapper):
     class ToMeDinoVisionTransformer(transformer_class):
         """
         Modifications:
@@ -197,6 +245,8 @@ def make_tome_class(transformer_class : _LinearClassifierWrapper):
 def apply_patch(
     model: _LinearClassifierWrapper, trace_source: bool = False, prop_attn: bool = True
 ):
+    assert isinstance(model, _LinearClassifierWrapper)
+
     ToMeDinoVisionTransformer = make_tome_class(model.__class__)
 
     model.__class__ = ToMeDinoVisionTransformer
@@ -213,6 +263,8 @@ def apply_patch(
         "distill_token": False,
     }
 
+    warn_prop_attn = False
+
     ### Iterate over backbone modules
     for module in model.backbone.modules():
         ### Note: order matters
@@ -225,10 +277,16 @@ def apply_patch(
 
         ### Note: order matters
         if isinstance(module, MemEffAttention):
-            if model._tome_info["prop_attn"]:
-                print("tome/patch/dinov2.py: Proportional attention will not be applied to MemEfficientAttention")
+            ### NOTE: Why is this not supported? memory_efficient_attention requires certain dimensionalities / memory alignment requirements
+            ### that seems to be finicky when we attempt to use memory efficient attention. See commented out section and do some testing to see this
+            ### Therefore, we ignore this flag if we are doing MemoryEfficientAttention
+            if prop_attn and not warn_prop_attn:
+                print(
+                    "hwtome/patch/dinov2.py: Proportional attention is ignored for MemEffAttention Blocks"
+                )
+                warn_prop_attn = True
             module.__class__ = ToMeDinoV2MemEffAttention
-            module._tome_info = model._tome_info
         elif isinstance(module, Attention):
             module.__class__ = ToMeDinoV2Attention
-            module._tome_info = model._tome_info
+
+    return model
